66efe1bef31f29b5e6b264bf97b3bf16f209d180
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2018-02-03 00:24:26 +0100

    minor

--- SAR_reducer.sh
+++ SAR_reducer.sh
@@ -12,7 +12,5 @@ set -x
 echo "@REDUCER_RUN $(date +%s) start reducing"
 output=${2:-.}/SAR_animation_$(date +%s).gif
 cd ${1:-.}
-ls -l .
-ls *.png
 convert -delay 90 -loop 0 *.png $output
 echo "@REDUCER_RUN $(date +%s) finish reducing"
